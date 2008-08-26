39d36c205081b93b8887788d97a1a41c93c594c5
atom <atom@1ca5e2b0-be53-0410-9c2d-c473b59480ba>
2008-08-26 08:40:46 +0000

    Darby Mitchell's additions.

--- build_tools/SCons/cxxtest.py
+++ build_tools/SCons/cxxtest.py
@@ -1,4 +1,5 @@
 # CxxTest builder by Gasper Azman
+# Modified by J. Darby Mitchell (08-22-2008)
 #
 # please send bugreports/praise/comments/criticism to
 # gasper.azman at gmail.com
@@ -20,35 +21,42 @@
 # This should be in a file called cxxtest.py somewhere in the toolpath.
 #
 # CHANGELOG:
-# 2008-08-23:
-#   John Mitchell of MIT proposed some nice changes regarding tool finding and
-#   reported a (bug) feature request where the cxxtest suite was installed as an
-#   rpm into the system include directories. Also, the environment variables can
-#   now be set at anytime in the environment, not just throught kwargs, because
-#   the defaults do not override the values set before tool initialization.
+# 2008-08-25: Gasper Azman and Darby Mitchell (MIT) refactored the tool to
+#    set defaults that don't override previously set environment variables, and 
+#    automatically search for the cxxtestgen Python script in the path and 
+#    project if it isn't specified by the user in the environment or kwargs.
+# 2008-08-23: Gasper Azman removed Perl support, since SCons is based on Python
+#    and CxxTest has plans to drop the cxxtestgen Perl script in the near future.
 # 2008-03-29:
-#   Fixed a bug introduced in the last version, where the script would fail if
-#   CXXFLAGS was never set.
+#    Fixed a bug introduced in the last version, where the script would fail if
+#    CXXFLAGS was never set.
 # 2008-03-23:
-#   Added CXXTEST_CXXFLAGS_REMOVE
-#   Bugfix: if only a single test was specified, scons check would do nothing
-#   Bugfix: test would not compile if '#' was not a part of path. Now added
-#   automatically.
+#    Added CXXTEST_CXXFLAGS_REMOVE
+#    Bugfix: if only a single test was specified, scons check would do nothing
+#    Bugfix: test would not compile if '#' was not a part of path. Now added
+#    automatically.
 # 2008-06-22:
-#   Bugfix: on windows, the script would not work because windows does not
-#   support running scripts without an explicit interpreter invocation
-#   (./script.py). 
-#   Patch kindly provided by Diego Nieto Cid (dnietoc at gmail dot com)
+#    Bugfix: on windows, the script would not work because windows does not
+#    support running scripts without an explicit interpreter invocation
+#    (./script.py). 
+#    Patch kindly provided by Diego Nieto Cid (dnietoc at gmail dot com)
 # 2008-06-25:
-#   Bugfix: Diego also provided a patch to his added functionality, so that
-#   now not all paths are imported, but just the path required to run the
-#   interpreter.
+#    Bugfix: Diego also provided a patch to his added functionality, so that
+#    now not all paths are imported, but just the path required to run the
+#    interpreter.
 #
-# Last changed: 2008-08-23 11:15:00 CEST
+# Last changed: 2008-06-25 23:58:40 CEST
 
 from SCons.Script import *
 from SCons.Builder import Builder
 from os import path
+
+# A few warning classes created to notify users of problems
+class ToolCxxTestWarning(SCons.Warnings.Warning):
+    pass
+
+SCons.Warnings.enableWarningClass(ToolCxxTestWarning)
+
 
 def UnitTest(env, target, source = [], **kwargs):
     cxxflags = ""
@@ -63,83 +71,103 @@ def UnitTest(env, target, source = [], **kwargs):
     env.AlwaysBuild(env['CXXTEST_TARGET'])
     return test
 
-def findScript(env):
-    return env.WhereIs('cxxtestgen.py', Dir(env['CXXTEST_DIR']).abspath, '')
 
-def findHeaderDir(env):
-    header = env.FindFile('TestSuite.h',[
-        os.path.normpath(env['CXXTEST_DIR']),
-        os.path.normpath(env['CXXTEST_DIR']+'/cxxtest/'),
-        '/usr/include/cxxtest/',
-        '/usr/include/'
-        ])
-    # the usual include is cxxtest/TestSuite.h, so return one directory less...
-    return os.path.normpath(header.dir.abspath+'/../');
+def isValidScriptPath(cxxtestgen):
+    """check keyword arg or environment variable locating cxxtestgen.py script"""
+       
+    if(cxxtestgen and cxxtestgen.endswith('.py') and path.exists(cxxtestgen) ):
+        return True
+    else:
+        SCons.Warnings.warn(ToolCxxTestWarning,
+                            "Invalid CXXTEST environment variable specified!")
+        return False
+    
+
+def findCxxTestGen(env):
+    """locate the cxxtestgen script by checking environment, path and project"""
+    
+    # check the SCons environment...
+    # Then, check the OS environment...
+    cxxtest = (env.get('CXXTEST', None) or
+               os.environ.get('CXXTEST', None)
+              )
+    
+    # If the user specified the location in the environment, make sure it was correct
+    if(cxxtest and isValidScriptPath(cxxtest) ):
+       return cxxtest
+    
+    # No valid environment variable found, so...
+    # Next, check the path...
+    # Finally, check the project
+    cxxtest = (env.WhereIs('cxxtestgen.py') or 
+               env.WhereIs('cxxtestgen.py', path=Dir(path.join('#', 'cxxtest') ).abspath) 
+              )
+    
+    if(cxxtest):
+        return cxxtest
+    else:
+        # If we weren't able to locate the cxxtestgen.py script, complain...
+        SCons.Warnings.warn(
+                ToolCxxTestWarning,
+                "Unable to locate cxxtestgen in environment, path or project!")
+        return None
+    
 
 def generate(env, **kwargs):
     """
     Accepted keyword arguments:
-    CXXTEST_DIR     - the directory the cxxtest library is in.
-                        Default: #/cxxtest, but /usr/include/cxxtest is also
-                        searched and will be used if #/cxxtest does not exist.
-                        This is used only as a searchpath for CXXTEST_HEADERS
-                        and CXXTEST.
-    CXXTEST         - the path to the cxxtestgen.py or cxxtestgen.pl.
-                        Default: CXXTEST_DIR/cxxtestgen.py.
-    CXXTEST_HEADERS - the directory the cxxtest headers are in.
-                        Default: CXXTEST_DIR
-    CXXTEST_PRINTER - the printer to use. Default: error-printer
-    CXXTEST_OPTS    - other options to pass to cxxtest. Default: ''
-    CXXTEST_SUFFIX  - the suffix of the test files. Default: '.t.h'
-    CXXTEST_TARGET  - the target to append the tests to. Default: check
+    CXXTEST         - the path to the cxxtestgen.py
+                        Default: searches SCons environment, OS environment,
+                        path and project in that order.
+    CXXTEST_PRINTER - the printer to use.  Default: error-printer
+    CXXTEST_OPTS    - other options to pass to cxxtest.  Default: ''
+    CXXTEST_SUFFIX  - the suffix of the test files.  Default: '.t.h'
+    CXXTEST_TARGET  - the target to append the tests to.  Default: check
     CXXTEST_CXXFLAGS_REMOVE - the flags that cxxtests can't compile with,
                               or give lots of warnings. Will be stripped.
                               Default: -pedantic -Weffc++
     CXXTEST_RUNNER  - how to run the script? (for windows compability :-)
-                        Default:os dependent: POSIX=empty;WINDOWS=(python|perl)
+                        Default: searches path for python
     ... and all others that Program() accepts, like CPPPATH etc.
     """
+
+    print "Loading CxxTest tool..."
+    
+    #If the user specified the path to CXXTEST, make sure it is correct
+    #otherwise, search for and set the default toolpath
+    if(not kwargs.has_key('CXXTEST') or not isValidScriptPath(kwargs['CXXTEST']) ):
+        env.SetDefault( CXXTEST  = findCxxTestGen(env) )
+
     #
-    # SET SCRIPT DEFAULTS
-    #
-    env.SetDefault( CXXTEST_DIR     = '#/cxxtest'        )
-    env.SetDefault( CXXTEST         = findScript(env)    )
-    env.SetDefault( CXXTEST_HEADERS = findHeaderDir(env) )
+    # Expected behavior: keyword arguments override environment variables;
+    # environment variables override default settings.
+    #          
     env.SetDefault( CXXTEST_PRINTER = 'error-printer'    )
     env.SetDefault( CXXTEST_OPTS    = ''                 )
     env.SetDefault( CXXTEST_SUFFIX  = '.t.h'             )
     env.SetDefault( CXXTEST_TARGET  = 'check'            )
     env.SetDefault( CXXTEST_CXXFLAGS_REMOVE = ['-pedantic','-Weffc++'] )
-    # this no longer supports perl (kind of obvious since SCons is based on
-    # python and the perl script will be dropped in the near future)
-    env.SetDefault( CXXTEST_RUNNER  = env.WhereIs('python') )
+    env.SetDefault( CXXTEST_RUNNER = env.WhereIs('python') )
     
-    #
-    # HANDLE kwargs...
-    #
-    env['CXXTEST_DIR']     = kwargs.get('CXXTEST_DIR', env['CXXTEST_DIR'] )
-    env['CXXTEST']         = kwargs.get('CXXTEST',     env['CXXTEST']     )
-    env['CXXTEST_HEADERS'] = kwargs.get('CXXTEST_HEADERS', env['CXXTEST_HEADERS'])
-    env['CXXTEST_RUNNER']  = kwargs.get('CXXTEST_RUNNER', env['CXXTEST_RUNNER'])
-    env['CXXTEST_PRINTER'] = kwargs.get('CXXTEST_PRINTER', env['CXXTEST_PRINTER']);
-    env['CXXTEST_OPTS']    = kwargs.get('CXXTEST_OPTS', env['CXXTEST_OPTS']);
-    env['CXXTEST_SUFFIX']  = kwargs.get('CXXTEST_SUFFIX', env['CXXTEST_SUFFIX']);
-    env['CXXTEST_TARGET']  = kwargs.get('CXXTEST_TARGET', env['CXXTEST_TARGET']);
-    env['CXXTEST_CXXFLAGS_REMOVE'] = Split(
-            kwargs.get(
-                'CXXTEST_CXXFLAGS_REMOVE',
-                env['CXXTEST_CXXFLAGS_REMOVE']
-                )
+    #Here's where keyword arguments are applied
+    apply(env.Replace, (), kwargs)
+    
+    cxxtest = env['CXXTEST']
+    if(cxxtest):
+        # Check to see if there is a 'cxxtest' subdirectory in the location where
+        # the script was found.  If so, assume that is the header directory, and  
+        # therefore the script directory should be included in the CPPPATH
+        if(path.exists(path.join(path.dirname(cxxtest), 'cxxtest') ) ):
+           env.AppendUnique(CPPPATH = path.dirname(cxxtest) )
+    
+        #
+        # Create the Builder (only if we have a valid cxxtestgen!)
+        #
+        cxxtest_builder = Builder(
+            action = "$CXXTEST_RUNNER $CXXTEST --$CXXTEST_PRINTER $CXXTEST_OPTS -o $TARGET $SOURCE",
+            suffix = ".cpp",
+            src_suffix = '$CXXTEST_SUFFIX'
             )
-
-    #
-    # CREATE THE BUILDER
-    #
-    cxxtest_builder = Builder(
-        action = "$CXXTEST_RUNNER $CXXTEST --$CXXTEST_PRINTER $CXXTEST_OPTS -o $TARGET $SOURCE",
-        suffix = ".cpp",
-        src_suffix = '$CXXTEST_SUFFIX'
-        )
 
     def CxxTest(env, target, source = [], **kwargs):
         """Usage:
@@ -158,8 +186,8 @@ def generate(env, **kwargs):
         if (source == []):
             source = Split(target + env['CXXTEST_SUFFIX'])
         sources = Split(source)
-        sources[0] = env.CxxTestCpp(sources[0])
-        env.AppendUnique(CPPPATH = ['$CXXTEST_HEADERS','#'])
+        sources[0] = env.CxxTestCpp(sources[0])    
+        env.AppendUnique(CPPPATH = '#')
 
         return UnitTest(env, target, source = sources, **kwargs)
 
